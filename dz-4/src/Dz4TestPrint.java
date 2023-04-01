68043ad9a157543c0e945892a5f9bbdd9ee588b8
ElenaS779 <pastushelena@gmail.com>
2023-04-01 21:30:21 +0200

    My homework dz-4

---
+++ dz-4/src/Dz4TestPrint.java
@@ -0,0 +1,8 @@
+public class Dz4TestPrint {
+    int a = 15;
+public int print(){
+    System.out.println("a = " + a);
+    return a;
+}
+
+}
---
+++ dz-4/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Dz4TestPrint test = new Dz4TestPrint();
+        System.out.println(String.format("returned value: %d ", test.print()));
+    }
+}
